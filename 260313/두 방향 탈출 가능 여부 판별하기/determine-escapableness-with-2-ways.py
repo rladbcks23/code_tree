cc35edff015b42bcad0a7c170fc2068c78e126f6
YuChan Kim <a01030163474@gmail.com>
2026-03-13 11:34:48 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 56ms, Memory: 17MB, Status: Wrong Answer - Codetree

---
+++ 260313/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
@@ -0,0 +1,22 @@
+n, m = map(int, input().split())
+grid = [list(map(int, input().split())) for _ in range(n)]
+
+# Please write your code here.
+visited = []
+delta = [(0, 1), (1, 0)]    # 우, 하
+
+
+def dfs(i, j):
+    if j == n-1:
+        return 1
+
+    for di, dj in delta:
+        ni, nj = i+di, j+dj
+        if 0 <= ni < n and 0 <= nj < n and grid[ni][nj] == 1:
+            return dfs(ni, nj)
+
+    return 0
+
+
+result = dfs(0, 0)
+print(result)
